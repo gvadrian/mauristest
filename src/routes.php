1f7df47a64aa356e424932c70adecff2cb8120dc
delawaredev <b.buhutskyi@lenal.biz>
2018-10-03 22:35:56 +0300

    deleted redundant files and code

--- src/routes.php
+++ src/routes.php
@@ -4,14 +4,8 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Controllers\UsersController;
 
-// Routes
-
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/', function() {
+   return 'Instruction for Mauris test task';
 });
 
 $app->get('/api/users', 'App\Controllers\UsersController:showAll');
